1da928928960b886a7eb10a97c4588ad5e44b3ee
Geshan Manandhar <geshan.manandhar@theiconic.com.au>
2020-11-23 22:22:40 +1100

    Post quotes API

--- app.js
+++ app.js
@@ -15,4 +15,12 @@ app.use(cookieParser());
 app.use('/', indexRouter);
 app.use('/quotes', quotesRouter);
 
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  console.error(err.message, err.stack);
+  res.status(statusCode).json({'message': err.message});
+  
+  return;
+})
+
 module.exports = app;
--- services/quotes.js
+++ services/quotes.js
@@ -17,6 +17,57 @@ async function getMultiple(page = 1){
   }
 }
 
+function validateCreate(quote) {
+  let messages = [];
+
+  console.log(quote);
+
+  if (!quote) {
+    messages.push('No object is provided');
+  }
+
+  if (!quote.quote) {
+    messages.push('Quote is empty');
+  }
+
+  if (!quote.author) {
+    messages.push('Quote is empty');
+  }
+
+  if (quote.quote && quote.quote.length > 255) {
+    messages.push('Quote cannot be longer than 255 characters');
+  }
+
+  if (quote.author && quote.author.length > 255) {
+    messages.push('Author name cannot be longer than 255 characters');
+  }
+
+  if (messages.length) {
+    let error = new Error(messages.join());
+    error.statusCode = 400;
+
+    throw error;
+  }
+}
+
+async function create(quote){
+  validateCreate(quote);
+
+  const result = await db.query(
+    'INSERT INTO quote (quote, author) VALUES (?, ?)', 
+    [quote.quote, quote.author]
+  );
+
+  let message = 'Error in creating quote';
+
+  if (result.affectedRows) {
+    message = 'Quote created successfully';
+  }
+
+  return {message};
+}
+
 module.exports = {
-  getMultiple
+  getMultiple,
+  create
 }
